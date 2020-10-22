5ccb5202f8761906c6dad6dd951d0bff5051fb61
pramodkatake-Continnum <pramod.katake@continnum.net>
2020-10-22 14:01:23 +0530

    Implemented PriorityFilter

--- filter.go
+++ filter.go
@@ -13,7 +13,7 @@ type DataSet struct {
 	CurRec   interface{}
 }
 
-//IsColumnNameValid : To validate Column Name.
+//IsColumnNameValid : To validate Column Name
 func IsColumnNameValid(Column *string, val reflect.Value) bool {
 	ColumnUp := strings.ToUpper(*Column)
 	var ColumnFound = false
@@ -27,7 +27,7 @@ func IsColumnNameValid(Column *string, val reflect.Value) bool {
 	return ColumnFound
 }
 
-//GetProfilesSubQueries :  Function to Get Sub Queries with ProfileIDs.
+//GetProfilesSubQueries :  Function to Get Sub Queries with ProfileIDs
 func GetProfilesSubQueries(filterConditions string) ([]string, error) {
 	subQuery := GetSubQueries(filterConditions, OPAND, OPOR)
 
@@ -175,13 +175,29 @@ func FilterProfiles(filterConditions string, sortCond string, val reflect.Value,
 
 //Filter : Filters data on given filterConditions.
 func Filter(filterConditions string, val reflect.Value, arraySlice []interface{}) ([]interface{}, error) {
-
+	var err error
 	arraySliceRS := make(map[int]DataSet)
 	for ind := range arraySlice {
 		arraySliceRS[ind] = DataSet{bMatched: false, CurRec: arraySlice[ind]}
 	}
 
 	filterConditions = CleanConditions(filterConditions)
+	arraySliceRS, err = markRecords(filterConditions, val, arraySliceRS)
+	if err != nil {
+		return nil, err
+	}
+
+	arraySlice = make([]interface{}, 0)
+	for l := 0; l < len(arraySliceRS); l++ {
+		if arraySliceRS[l].bMatched {
+			arraySlice = append(arraySlice, arraySliceRS[l].CurRec)
+		}
+	}
+	return arraySlice, nil
+}
+
+//markRecords : mark records to be filtered based on filterConditions.
+func markRecords(filterConditions string, val reflect.Value, arraySlice map[int]DataSet) (map[int]DataSet, error) {
 	subQuery := GetSubQueries(filterConditions, OPAND+STARTBRACE, OPOR+STARTBRACE)
 	for i := range subQuery {
 		var LOP, Key, Column, Op, CurQuery string
@@ -219,19 +235,19 @@ func Filter(filterConditions string, val reflect.Value, arraySlice []interface{}
 		if !ColumnFound {
 			return nil, errors.New("Filter [" + Column + "] No such column exist!!!")
 		}
-		for k := 0; k < len(arraySliceRS); k++ {
+
+		for k := 0; k < len(arraySlice); k++ {
 			bConsiderMe := false
 			if LOP == OPBLANK || LOP == OPOR {
-				bConsiderMe = !arraySliceRS[k].bMatched
+				bConsiderMe = !arraySlice[k].bMatched
 			} else if LOP == OPAND {
-				bConsiderMe = arraySliceRS[k].bMatched
+				bConsiderMe = arraySlice[k].bMatched
 			}
 
 			var err error
-
 			if bConsiderMe {
 				bMatched := false
-				CurField := reflect.Indirect(reflect.ValueOf(arraySliceRS[k].CurRec)).FieldByName(Column)
+				CurField := reflect.Indirect(reflect.ValueOf(arraySlice[k].CurRec)).FieldByName(Column)
 
 				if isPrimitive(CurField) {
 					if bMatched, err = processPrimitive(CurField, Key, Op); err != nil {
@@ -256,19 +272,10 @@ func Filter(filterConditions string, val reflect.Value, arraySlice []interface{}
 					}
 
 				}
-
-				arraySliceRS[k] = DataSet{bMatched: bMatched, CurRec: arraySliceRS[k].CurRec}
+				arraySlice[k] = DataSet{bMatched: bMatched, CurRec: arraySlice[k].CurRec}
 			}
 		}
 	}
-
-	arraySlice = make([]interface{}, 0)
-	for l := 0; l < len(arraySliceRS); l++ {
-		if arraySliceRS[l].bMatched {
-			arraySlice = append(arraySlice, arraySliceRS[l].CurRec)
-		}
-	}
-
 	return arraySlice, nil
 }
 
@@ -400,4 +407,189 @@ func processPrimitive(CurField reflect.Value, Key string, Op string) (bMatched b
 	}
 
 	return bMatched, nil
+}
+
+//PriorityFilter : Filters data based on priority of given complex filterConditions.
+func PriorityFilter(filterConditions string, val reflect.Value, arraySlice []interface{}) ([]interface{}, error) {
+	var err error
+	arraySliceRS := make(map[int]DataSet)
+	for ind := range arraySlice {
+		arraySliceRS[ind] = DataSet{bMatched: false, CurRec: arraySlice[ind]}
+	}
+
+	filterConditions = CleanConditions(filterConditions)
+	arraySliceRS, err = ResolveFilterConditions(filterConditions, val, arraySliceRS)
+	if err != nil {
+		return nil, err
+	}
+
+	arraySlice = make([]interface{}, 0)
+	for l := 0; l < len(arraySliceRS); l++ {
+		if arraySliceRS[l].bMatched {
+			arraySlice = append(arraySlice, arraySliceRS[l].CurRec)
+		}
+	}
+	return arraySlice, nil
+}
+
+//ResolveFilterConditions : Resolve Filter Conditions based on priority
+func ResolveFilterConditions(filter string, val reflect.Value, dataMap map[int]DataSet) (map[int]DataSet, error) {
+	var err error
+	dataMapRet := make(map[int]DataSet)
+	var bExtractForward, bIsComplexFilter, bImbalance bool
+	var pendingFilter, curCondition, curOperator string
+	if strings.HasPrefix(filter, STARTBRACE+STARTBRACE) && strings.HasSuffix(filter, ENDBRACE+ENDBRACE) {
+		iBalancePos, iLength := GetBalancePosition(filter)
+		if iBalancePos == iLength-1 {
+			filter = filter[1 : len(filter)-1]
+		} else {
+			bImbalance = true
+			curCondition = filter[:iBalancePos+1]
+			curOperator = filter[iBalancePos+1 : iBalancePos+2]
+			pendingFilter = filter[iBalancePos+2:]
+		}
+	}
+
+	if bImbalance {
+		dataMapCopy := make(map[int]DataSet)
+		for k, v := range dataMap {
+			dataMapCopy[k] = v
+		}
+
+		dataMap, err = ResolveFilterConditions(pendingFilter, val, dataMap)
+		if err != nil {
+			return nil, err
+		}
+		dataMapCopy, err = ResolveFilterConditions(curCondition, val, dataMapCopy)
+		if err != nil {
+			return nil, err
+		}
+		dataMapRet, err = MergeFilterResults(dataMap, dataMapCopy, curOperator)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		if strings.HasPrefix(filter, STARTBRACE) && strings.HasSuffix(filter, ENDBRACE+ENDBRACE) {
+			bExtractForward = true
+			bIsComplexFilter = true
+		} else if strings.HasPrefix(filter, STARTBRACE+STARTBRACE) && strings.HasSuffix(filter, ENDBRACE) {
+			bExtractForward = false
+			bIsComplexFilter = true
+		} else if strings.Contains(filter, STARTBRACE+STARTBRACE) {
+			bExtractForward = true
+			bIsComplexFilter = true
+		} else if strings.Contains(filter, ENDBRACE+ENDBRACE) {
+			bExtractForward = false
+			bIsComplexFilter = true
+		} else {
+			bIsComplexFilter = false
+		}
+
+		if bIsComplexFilter {
+			pendingFilter, curCondition, curOperator = ExtractConditions(filter, bExtractForward)
+			dataMapCopy := make(map[int]DataSet)
+			for k, v := range dataMap {
+				dataMapCopy[k] = v
+			}
+
+			dataMap, err = ResolveFilterConditions(pendingFilter, val, dataMap)
+			if err != nil {
+				return nil, err
+			}
+			dataMapCopy, err = markRecords(curCondition, val, dataMapCopy)
+			if err != nil {
+				return nil, err
+			}
+			dataMapRet, err = MergeFilterResults(dataMap, dataMapCopy, curOperator)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			curCondition = filter
+			dataMapRet, err = markRecords(curCondition, val, dataMap)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return dataMapRet, nil
+}
+
+//ExtractConditions : Extracts simplest conditions from complex conditions.
+func ExtractConditions(sFilter string, bExtractForward bool) (pendingFilter string, curCondition string, curOperator string) {
+	var i, iAnd, iOr int
+	i = -1
+	if bExtractForward {
+		iAnd = strings.Index(sFilter, OPAND)
+		iOr = strings.Index(sFilter, OPOR)
+	} else {
+		iAnd = strings.LastIndex(sFilter, OPAND)
+		iOr = strings.LastIndex(sFilter, OPOR)
+	}
+
+	if iAnd > -1 && iOr > -1 {
+		i = iOr
+		if bExtractForward && (iAnd < iOr) {
+			i = iAnd
+		} else if !bExtractForward && (iAnd > iOr) {
+			i = iAnd
+		}
+	} else if iAnd > -1 {
+		i = iAnd
+	} else if iOr > -1 {
+		i = iOr
+	}
+
+	if bExtractForward {
+		curCondition = sFilter[0:i]
+		curOperator = sFilter[i : i+1]
+		pendingFilter = sFilter[i+1:]
+	} else {
+		curCondition = sFilter[i+1:]
+		curOperator = sFilter[i : i+1]
+		pendingFilter = sFilter[:i]
+	}
+	return pendingFilter, curCondition, curOperator
+}
+
+//MergeFilterResults : Merges Filter Conditions results
+func MergeFilterResults(dataMapLeft, dataMapRight map[int]DataSet, curOperation string) (map[int]DataSet, error) {
+	if len(dataMapLeft) != len(dataMapRight) {
+		return nil, errors.New("MergeFilterResults Length Check Failed!!!")
+	}
+
+	if curOperation != OPOR && curOperation != OPAND {
+		return nil, errors.New("Filter Invalid Operator [" + curOperation + "] Applied!!!")
+	}
+
+	for i := 0; i < len(dataMapLeft); i++ {
+		bMatched := false
+		if curOperation == OPOR {
+			bMatched = dataMapLeft[i].bMatched || dataMapRight[i].bMatched
+		} else if curOperation == OPAND {
+			bMatched = dataMapLeft[i].bMatched && dataMapRight[i].bMatched
+		}
+		dataMapLeft[i] = DataSet{bMatched: bMatched, CurRec: dataMapLeft[i].CurRec}
+	}
+	return dataMapLeft, nil
+}
+
+//GetBalancePosition : Gets balancing position for condition.
+func GetBalancePosition(sFilter string) (iBalancePos int, iLength int) {
+	var iStart, iEnd int
+	iLength = len(sFilter)
+	for i := 0; i < iLength; i++ {
+		if sFilter[i:i+1] == STARTBRACE {
+			iStart++
+		} else if sFilter[i:i+1] == ENDBRACE {
+			iEnd++
+		}
+
+		if (iStart == iEnd) && (iStart != 0) {
+			iBalancePos = i
+			break
+		}
+	}
+
+	return iBalancePos, iLength
 }
